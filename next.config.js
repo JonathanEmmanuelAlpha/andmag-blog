a264b87325ee98ec0dbeabe76c5da9090a7578e9
jonathan emmanuel tonye <emmanueljonathant453@gmail.com>
2023-03-06 00:09:12 +0100

    Project ready for production

--- next.config.js
+++ next.config.js
@@ -7,18 +7,11 @@ const nextConfig = {
     localeDetection: false,
   },
   images: {
-    domains: ["localhost", "firebasestorage.googleapis.com"],
+    domains: [
+      "https://andmag-ground.vercel.app",
+      "firebasestorage.googleapis.com",
+    ],
   },
 };
 
 module.exports = nextConfig;
-
-const withPWA = require("next-pwa");
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    register: true,
-    disable: process.env.NODE_ENV === "development",
-    skipWaiting: true,
-  },
-});
