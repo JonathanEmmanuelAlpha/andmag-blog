8ccef3a6ef2710c3f99874380ad04419b7336d21
JonathanEmmanuelAlpha <80617171+JonathanEmmanuelAlpha@users.noreply.github.com>
2023-12-07 14:54:39 +0100

    Update Header.jsx
    Corecting meta tag

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -65,7 +65,7 @@ export default function Header({
             content="_vWfdPyu3FhlyaU6-0SE_aaEecwEICwLk4yhqqKxsX4"
           />
         )}
-        <meta name="surfe.pro" content="5f0f37988268be0582b791da7eff09c5">
+        <meta name="surfe.pro" content="5f0f37988268be0582b791da7eff09c5" />
       </Head>
       <header className={styles.container}>
         <AppICon onOpen={onOpen} />
