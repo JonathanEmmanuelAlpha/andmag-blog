3f74ac905b5984db0320e04b73f94466f2de1610
JonathanEmmanuelAlpha <80617171+JonathanEmmanuelAlpha@users.noreply.github.com>
2023-12-07 15:59:18 +0100

    Update Header.jsx
    Adding Surfe.be script

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -66,6 +66,7 @@ export default function Header({
           />
         )}
         <meta name="surfe.pro" content="5f0f37988268be0582b791da7eff09c5" />
+        <script>(adsurfebe = window.adsurfebe || []).push({});</script>
       </Head>
       <header className={styles.container}>
         <AppICon onOpen={onOpen} />
