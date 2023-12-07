e06bf5d86b1ebcdeeea6df6c74070053f639f33e
JonathanEmmanuelAlpha <80617171+JonathanEmmanuelAlpha@users.noreply.github.com>
2023-12-07 15:45:55 +0100

    Update contact.jsx
    correcting the class attr

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -79,7 +79,7 @@ export default function Contact() {
       <div className={styles.wrapper}>
         <section className={styles.info}>
           <h1>Entrez en contact</h1>
-          <ins class="surfe-be" data-sid="387090"></ins>
+          <ins className="surfe-be" data-sid="387090"></ins>
           <p>
             {
               "Si vous souhaité entré en contact, parler nous d'un projet ou dites juste salut. Si vous souhaité nous envoyer un mail, remplissez le formulaire ci-dessous ou cliquez sur "
