2a97511f055c52c4abf5acbb1337889dd8c4aecd
Tonye Mayilla Emmanuel Jonathan <80617171+JonathanEmmanuelAlpha@users.noreply.github.com>
2024-09-23 16:06:24 +0100

    Change manual linking

--- components/nav-bar/Account.jsx
+++ components/nav-bar/Account.jsx
@@ -2,11 +2,10 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import styles from "../../styles/nav-bar/Account.module.css";
-import { domainName } from "../links/AwesomeLink.type";
 
 export default function Account({ photo, pseudo }) {
   return photo || pseudo ? (
-    <Link href={`${domainName}/account/profile`}>
+    <Link href={`/account/profile`}>
       <a className={styles.wrapper}>
         {photo && (
           <Image
